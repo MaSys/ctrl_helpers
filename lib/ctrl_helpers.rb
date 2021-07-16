4978533d0ac094861c26e0ca6efe6826b7b435c8
Yaser Almasri <yaser@masys.mx>
2021-07-15 22:18:48 -0500

    init

---
+++ lib/ctrl_helpers.rb
@@ -0,0 +1,6 @@
+require "ctrl_helpers/version"
+
+module CtrlHelpers
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ctrl_helpers/version.rb
@@ -0,0 +1,3 @@
+module CtrlHelpers
+  VERSION = "0.1.0"
+end
